939c5a995055d3fcd31943e2362295398cd31c3b
Wakaba <wakaba@suikawiki.org>
2023-08-15 11:07:55 +0900

    45min

--- convert.pl
+++ convert.pl
@@ -49,7 +49,7 @@ while (<>) {
     my $d_file = qq{/app/data/$_};
 
     my $elapsed = time - $start_time;
-    if ($elapsed > 1*60) {
+    if ($elapsed > 45*60) {
       warn "Elapsed: $elapsed s, terminated\n";
       warn "Converted: $converted, Skipped: $skipped\n";
       exit;
