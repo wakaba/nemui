f5407638be7828ad466591beeb529eb35c89eba8
Wakaba <wakaba@suikawiki.org>
2023-08-16 14:34:44 +0900

    rename

--- move-modmag.pl
+++ move-modmag.pl
@@ -1,17 +1,11 @@
 use strict;
 use warnings;
 
-my $i = 0;
-sub run (@) {
-  print STDERR "Command: |@_|\n" if ($i++ % 1000) == 0;
-  (system @_) == 0 or die "Failed: $?";
-}
-
 while (<>) {
   s/[\x0D\x0A]+$//g;
   if (m{/([a-z0-9-]+)\.jpg/([0-9-]+\.png)$}) {
     my $in_file = $_;
     my $out_file = "local/data/modmag/$1-$2";
-    run 'mv', $in_file, $out_file;
+    rename $in_file, $out_file;
   }
 }
--- move-tensho.pl
+++ move-tensho.pl
@@ -1,17 +1,11 @@
 use strict;
 use warnings;
 
-my $i = 0;
-sub run (@) {
-  print STDERR "Command: |@_|\n" if ($i++ % 1000) == 0;
-  (system @_) == 0 or die "Failed: $?";
-}
-
 while (<>) {
   s/[\x0D\x0A]+$//g;
   if (m{/U\+[0-9A-F]+_(\w+\.png)}) {
     my $in_file = $_;
     my $out_file = "local/data/tensho/$1";
-    run 'mv', $in_file, $out_file;
+    rename $in_file, $out_file;
   }
 }
