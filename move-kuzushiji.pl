e4023dd189c6501393829bd03ffb8e330e6e7bb9
Wakaba <wakaba@suikawiki.org>
2023-08-16 15:55:18 +0900

    test

--- move-kuzushiji.pl
+++ move-kuzushiji.pl
@@ -4,9 +4,9 @@ use warnings;
 my $created = {};
 while (<>) {
   s/[\x0D\x0A]+$//g;
-  if (m{/U\+[0-9A-F]+_([\w-]+\.png)}) {
+  if (m{/U\+[0-9A-F]+_(([0-9a-z]+)[\w-]+\.png)}) {
     my $in_file = $_;
-    my $out_file = "local/data/kuzushiji/".(substr $1, 0, 2)."/$1";
+    my $out_file = "local/data/kuzushiji/".$2."/$1";
     my $out_dir = $out_file;
     $out_dir =~ s{[^/]+$}{};
     mkdir $out_dir unless $created->{$out_dir}++;
