56ed4d80f98c97214f37f0c07ec0ad751bdbf8d1
Wakaba <wakaba@suikawiki.org>
2023-08-16 15:34:03 +0900

    test

--- move-kuzushiji.pl
+++ move-kuzushiji.pl
@@ -1,11 +1,15 @@
 use strict;
 use warnings;
 
+my $created = {};
 while (<>) {
   s/[\x0D\x0A]+$//g;
   if (m{/U\+[0-9A-F]+_([\w-]+\.png)}) {
     my $in_file = $_;
-    my $out_file = "local/data/kuzushiji/$1";
+    my $out_file = "local/data/kuzushiji/".(substr $1, 0, 2)."/$1";
+    my $out_dir = $out_file;
+    $out_dir =~ s{[^/]+$}{};
+    mkdir $out_dir unless $created->{$out_dir}++;
     rename $in_file, $out_file;
   }
 }
