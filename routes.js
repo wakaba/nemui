50b38bc8cdfb13180d1f519e9f5426564e0fb31a
Wakaba <wakaba@suikawiki.org>
2024-06-19 13:35:54 +0900

    updated

--- routes.js
+++ routes.js
@@ -54,6 +54,27 @@
     return {distance2: minD, index: minDi, point: points[minDi]};
   } // getNearest
 
+  // lat ~ +-90 not supported
+  function lineDistance (p, s, e) {
+    let v = Math.pow (s.lat - e.lat, 2) + Math.pow (s.lon - e.lon, 2);
+    if (v === 0) { // s == e
+      return Math.pow (s.lat - p.lat, 2) + Math.pow (s.lon - p.lon, 2);
+    } else {
+      let t = (s.lat - e.lat) * (s.lat - p.lat) + (s.lon - e.lon) * (s.lon - p.lon);
+      t /= v;
+      if (t < 0) {
+        return Math.pow (s.lat - p.lat, 2) + Math.pow (s.lon - p.lon, 2);
+      } else if (1 < t) {
+        return Math.pow (e.lat - p.lat, 2) + Math.pow (e.lon - p.lon, 2);
+      } else {
+        let x = s.lat - t * (s.lat - e.lat);
+        let y = s.lon - t * (s.lon - e.lon);
+        return Math.pow (x - p.lat, 2) + Math.pow (y - p.lon, 2);
+      }
+    }
+  } // lineDistance
+
+
   function recompute (pointsList, radius) {
     let start = performance.now ();
 
@@ -89,33 +110,111 @@
     }
     let thresholdC = threshold * 3*3;
     let thresholdCP = thresholdC;
+
+    {
+      let rdp = (points, start, end, list, depth) => {
+        if (end - start <= 2 || depth > 100000) {
+          for (let i = start + 1; i <= end - 1; i++) {
+            list.push (i);
+          }
+          return;
+        }
+      
+        let max = 0;
+        let maxI = start + 1;
+        for (let i = start + 1; i <= end - 1; i++) {
+          let d = lineDistance (points[i], points[start], points[end]);
+          if (d > max) {
+            max = d;
+            maxI = i;
+          }
+        }
+        list.push (maxI);
+        let l1 = rdp (points, start, maxI, list, depth+1);
+        let l2 = rdp (points, maxI, end, list, depth+1);
+      }; // rdp
+
+      let list = [];
+      if (pointsList[0].length >= 1) {
+        list.push (0);
+        if (pointsList[0].length >= 2) {
+          list.push (pointsList[0].length-1);
+          rdp (pointsList[0], 0, pointsList[0].length-1, list, 0);
+        }
+      }
+      
+      let max = list.length - 1;
+      for (let i = 0; i <= max; i++) {
+        pointsList[0][list[i]].origIndex = list[i];
+        pointsList[0][list[i]].pointImportance = (max - i) / max;
+      }
+    }
+    let basePoints;
+    {
+      let rdp = (points, start, end, th, needMore) => {
+        if (end - start <= 2) return points.slice (start, end + 1);
+
+        let maxImportance = -1;
+        let maxImportanceI = start + 1;
+        let thD = false;
+        for (let i = start + 1; i <= end - 1; i++) {
+          if (points[i].confluential) {
+            maxImportance = Infinity;
+            maxImportanceI = i;
+            thD = true;
+            break;
+          }
+          if (thD ||
+              lineDistance (points[i], points[start], points[end]) > th) {
+            thD = true;
+          }
+
+          if (maxImportance < points[i].pointImportance) {
+            maxImportance = points[i].pointImportance;
+            maxImportanceI = i;
+          }
+        }
+        if (thD ||
+            (end - start > 1 ? needMore (points[start], points[end]) : 0)) {
+          let l1 = rdp (points, start, maxImportanceI, th, needMore);
+          let l2 = rdp (points, maxImportanceI, end, th, needMore);
+          return l1.concat (l2.slice (1));
+        } else {
+          return [points[start], points[end]];
+        }
+      }; // rdp
+
+      let t1 = threshold /2/2;
+      let t = threshold;
+      basePoints = rdp (pointsList[0], 0, pointsList[0].length-1, t1, (p1, p2) => {
+        return distance2 (p1, p2) > t;
+      });
+    }
     
     let augPoints = [];
     let confluentialPoints = [];
-    if (pointsList[0].length) {
-      pointsList[0][0].origIndex = 0;
-      pointsList[0][0].index = 0;
-      augPoints.push (pointsList[0][0]);
-      if (pointsList[0][0].confluential) {
+    if (basePoints.length) {
+      basePoints[0].index = 0;
+      augPoints.push (basePoints[0]);
+      if (basePoints[0].confluential) {
         confluentialPoints.push (0);
       }
     }
-    for (let i = 1; i < pointsList[0].length; i++) {
-      let d = distance2 (pointsList[0][i-1], pointsList[0][i]);
+    for (let i = 1; i < basePoints.length; i++) {
+      let d = distance2 (basePoints[i-1], basePoints[i]);
       if (d > threshold) {
         let n = Math.floor (Math.pow (d, 0.5) / Math.pow (threshold, 0.5)) + 1;
         for (let j = 1; j < n; j++) {
-          let p = {lat: (pointsList[0][i].lat*j+(pointsList[0][i-1].lat*(n-j)))/n,
-                   lon: (pointsList[0][i].lon*j+(pointsList[0][i-1].lon*(n-j)))/n,
+          let p = {lat: (basePoints[i].lat*j+(basePoints[i-1].lat*(n-j)))/n,
+                   lon: (basePoints[i].lon*j+(basePoints[i-1].lon*(n-j)))/n,
                    additional: true};
           p.index = augPoints.length;
           augPoints.push (p);
         }
       }
-      pointsList[0][i].origIndex = i;
-      pointsList[0][i].index = augPoints.length;
-      augPoints.push (pointsList[0][i]);
-      if (pointsList[0][i].confluential) {
+      basePoints[i].index = augPoints.length;
+      augPoints.push (basePoints[i]);
+      if (basePoints[i].confluential) {
         confluentialPoints.push (augPoints.length-1);
       }
     }
@@ -149,43 +248,7 @@
           useThC[k] = true;
         }
       }
-    });                     
-
-    let dataPoints = [];
-    let maxBaseIndex = Infinity;
-    {
-      let forced = pointsList[2].slice ().sort ((a, b) => a.timestamp - b.timestamp);
-      let o = 0;
-      let prevFP;
-      for (let p of pointsList[1]) {
-        while (forced.length && forced[0].timestamp < p.timestamp) {
-          let fp = forced.shift ();
-          let bp = pointsList[0][fp.baseOrigIndex];
-          fp.index = dataPoints.length;
-          if (bp) fp.baseIndex = bp.index;
-          dataPoints.push (fp);
-          if (prevFP) {
-            prevFP.maxBaseIndex = bp ? fp.baseIndex : Infinity;
-          } else {
-            maxBaseIndex = bp ? fp.baseIndex : Infinity;
-          }
-          prevFP = fp;
-        }
-        p.index = dataPoints.length;
-        p.origIndex = o++;
-        dataPoints.push (p);
-      }
-      while (forced.length) {
-        let fp = forced.shift ();
-        let bp = pointsList[0][fp.baseOrigIndex];
-        fp.index = dataPoints.length;
-        if (bp) fp.baseIndex = bp.index;
-        dataPoints.push (fp);
-        if (prevFP) prevFP.maxBaseIndex = bp ? fp.baseIndex : Infinity;
-        prevFP = fp;
-      }
-      if (prevFP) prevFP.maxBaseIndex = Infinity;
-    }
+    });
     
     let nears = [];
     for (let i = 0; i < augPoints.length; i++) {
@@ -255,11 +318,67 @@
       }
     }
     phases = phases.reverse ();
+    let computeBase = performance.now () - start;
+
+    let dataPoints = [];
+    let maxBaseIndex = Infinity;
+    {
+      let forced = pointsList[2].slice ().sort ((a, b) => a.timestamp - b.timestamp);
+      let o = 0;
+      let prevFP;
+      for (let p of pointsList[1]) {
+        while (forced.length && forced[0].timestamp < p.timestamp) {
+          let fp = forced.shift ();
+          let bp = pointsList[0][fp.baseOrigIndex];
+          fp.index = dataPoints.length;
+          if (bp) fp.baseIndex = bp.index;
+          dataPoints.push (fp);
+          if (prevFP) {
+            prevFP.maxBaseIndex = bp ? fp.baseIndex : Infinity;
+          } else {
+            maxBaseIndex = bp ? fp.baseIndex : Infinity;
+          }
+          prevFP = fp;
+        }
+        p.index = dataPoints.length;
+        p.origIndex = o++;
+        dataPoints.push (p);
+      }
+      while (forced.length) {
+        let fp = forced.shift ();
+        let bp = pointsList[0][fp.baseOrigIndex];
+        fp.index = dataPoints.length;
+        if (bp) fp.baseIndex = bp.index;
+        dataPoints.push (fp);
+        if (prevFP) prevFP.maxBaseIndex = bp ? fp.baseIndex : Infinity;
+        prevFP = fp;
+      }
+      if (prevFP) prevFP.maxBaseIndex = Infinity;
+    }
 
     let d2b = [];
+    let reverted = [];
     {
       let currentI = -1;
-      J: for (let j = 0; j < dataPoints.length; j++) {
+      let j;
+      let revertable = null;
+      let checkRevertable = (ok) => {
+        if (revertable) {
+          if (ok) {
+            revertable = null;
+          } else {
+            if (revertable.failed++ > 2) {
+              reverted.push ([currentI, revertable.currentI]);
+              currentI = revertable.currentI;
+              delete d2b[revertable.j].effectiveIndex;
+              delete d2b[revertable.j].phase;
+              j = revertable.j + 1;
+              revertable = null;
+            }
+          }
+        }
+      }; // checkRevertable
+      J: for (j = 0; j < dataPoints.length; j++) {
         let dp = dataPoints[j];
         if (dp.baseIndex != null) {
           let link = d2b[j] = {dataIndex: j, effectiveIndex: dp.baseIndex,
@@ -267,7 +386,7 @@
                                phase: i2p[dp.baseIndex],
                                forced: dp.source};
           maxBaseIndex = dp.maxBaseIndex;
-          continue J;
+          checkRevertable (true); continue J;
         }
         
         let nearest = getNearest (augPoints, dp);
@@ -277,29 +396,30 @@
 
           let candI = null;
           N: for (let n of nears[i]) {
-            if (n[1] === i) {
+            if (n[1] === i && false) {
               if (currentI < i) {
                 candI = i;
                 break;
               } else if (n[0] <= currentI) {
                 if (currentI <= n[2]) {
-                  continue J;
+                  link.unusedIndex = n[1];
+                  checkRevertable (false); continue J;
                 } else {
                   continue N;
                 }
               }
 
               if (i2p[currentI] === i2p[n[1]]) {
-                continue J;
+                link.unusedIndex = n[1];
+                checkRevertable (false); continue J;
               }
-
             } else {
               if (i2p[currentI] === i2p[n[0]] && currentI < n[1]) {
                 candI = n[1];
                 break;
               }
-              
-              if (currentI < n[2]) {
+
+              if (currentI <= n[2]) {
                 let minD = Infinity;
                 let minDi = -1;
                 let minDi2 = -1;
@@ -318,13 +438,26 @@
                   candI = minDi2;
                   break;
                 } else if (n[0] <= currentI) {
-                  continue J;
+                  if (currentI === n[2]) {
+                    continue N;
+                  } else if (currentI < n[2] &&
+                             distance2 (augPoints[currentI+1], dp) < threshold) {
+                    candI = currentI + 1;
+                    break;
+                  } else {
+                    link.unusedIndex = n[1];
+                    checkRevertable (false); continue J;
+                  }
                 }
               }
             }
           } // N
           if (candI !== null) {
+            let hasNonSmall = false;
             if (i2p[currentI] < i2p[candI]) {
+              // Check whether the current phase can be completed or not
+              // in case the segment can be revisited
+              let checkEndI = candI;
               N: for (let n of nears[currentI]) {
                 let c = false;
                 if (i2p[currentI] < i2p[n[1]] && i2p[n[1]] < i2p[candI]) {
@@ -332,19 +465,37 @@
                 } else if (i2p[currentI] <= i2p[n[2]] &&
                            i2p[n[1]] < i2p[candI] && candI <= n[2]) {
                   c = true;
-                } else if (i2p[n[2]] <= i2p[currentI]) {
+                } else if (i2p[n[0]] <= i2p[currentI] &&
+                           i2p[currentI] <= i2p[n[2]]) {
+                  if (nears[currentI] > 1) {
+                    c = true;
+                  } else {
+                    c = true;
+                    checkEndI = n[2];
+                  }
+                } else if (i2p[n[2]] < i2p[currentI]) {
                   continue N;
-                } else if (i2p[candI] < i2p[n[0]]) {
-                  continue N;
+                } else if (candI < n[0]) {
+                  break N;
+                } else {
+                  c = true;
+                  checkEndI = n[2];
                 }
                 if (c) {
                   if (0 <= i2p[n[1]] - i2p[currentI] &&
                       i2p[n[1]] - i2p[currentI] < 10) {
                     let small = true;
+                    let kk = [];
                     for (let k = i2p[currentI]; k <= i2p[n[1]]; k++) {
-                      if (phases[k][1] - phases[k][0] < 10) {
-                        let l = Math.floor ((phases[k][0] + phases[k][1]) / 2);
-                        if ((currentI <= l ? distances[l][currentI] : distances[currentI][l]) < threshold*4) {
+                      kk.push (phases[k]);
+                    }
+                    kk[0] = [currentI, kk[0][1]];
+                    let kkLast = kk[kk.length-1] = kk.at (-1).slice ();
+                    if (kkLast[1] > checkEndI) kkLast[1] = checkEndI;
+                    for (let ph of kk) {
+                      if (ph[1] - ph[0] < 10) {
+                        let l = Math.floor ((ph[0] + ph[1]) / 2);
+                        if ((currentI <= l ? distances[l][currentI] : distances[currentI][l]) < threshold*3*3) {
                           //
                         } else {
                           small = false;
@@ -354,30 +505,58 @@
                         small = false;
                         break;
                       }
+                    } // k
+                    if (!small) {
+                      //checkRevertable (false); continue J;
+                      hasNonSmall = true;
                     }
-                    if (!small) continue J;
-
                   } else {
-                    continue J;
+                    checkRevertable (false); continue J;
                   }
                 } else {
-                  //continue J;
+                  //
                 } // c
-              }
-            }
+              } // N
+            } // phase to be changed
             if (currentI !== candI && candI < maxBaseIndex) {
-              link.effectiveIndex = candI;
-              currentI = candI;
-              link.phase = i2p[candI];
+              if (hasNonSmall || i2p[candI] - i2p[currentI] > 5) {
+                if (revertable) {
+                  revertable.failed = Infinity;
+                  checkRevertable (false);
+                  continue J;
+                }
+                revertable = {
+                  failed: 0,
+                  currentI, j,
+                };
+                link.effectiveIndex = candI;
+                currentI = candI;
+                link.phase = i2p[candI];
+                continue J;
+              } else {
+                link.effectiveIndex = candI;
+                currentI = candI;
+                link.phase = i2p[candI];
+                checkRevertable (true); continue J;
+              }
+            } else {
+              link.unusedIndex = candI;
+              checkRevertable (false); continue J;
             }
-            continue J;
           } // candI
-        }
+        } // nearest
+        checkRevertable (false); continue J;
       } // J
+      if (revertable) {
+        revertable.failed = Infinity;
+        checkRevertable (false);
+      }
     }
 
+    let done = performance.now ();
     return {
-      elapsed: {start, recompute: performance.now () - start},
+      elapsed: {start, computeBase, recompute: done - start,
+                computeData: done - start - computeBase},
       thresholdMInput: radius,
       threshold,
       thresholdC,
@@ -388,6 +567,7 @@
       basePhases: phases,
       dataPoints,
       dataToBase: d2b,
+      reverted,
     };
   } // recompute
 
@@ -422,6 +602,7 @@
       urls.routes = new URL ('info.json', urls.routes);
       urls.results = new URL ('results.json', urls.routes);
     }
+    let fetchStart = performance.now ();
     return Promise.all ([
       fetch (urls.routes).then (res => {
         if (res.status !== 200) throw res;
@@ -472,6 +653,7 @@
         routePoints: pp,
         forcedPoints: forced,
         teamIds,
+        elapsed: performance.now () - fetchStart,
       };
     });
   } // fetchIbuki
